893ad6e3b9a139de89be616bfb20c23ac4cef10b
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-25 13:58:04 +0300

    updated: increase score

--- app/src/main/java/com/ismailmesutmujde/catchthekenny/MainActivity.java
+++ app/src/main/java/com/ismailmesutmujde/catchthekenny/MainActivity.java
@@ -1,15 +1,33 @@
 package com.ismailmesutmujde.catchthekenny;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timeText;
+    TextView scoreText;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // initialize
+        timeText = (TextView) findViewById(R.id.timeText);
+        scoreText = (TextView) findViewById(R.id.scoreText);
+
+        score = 0;
+
+    }
+
+    public void increaseScore(View view) {
+        score++;  // score = score + 1;
+        scoreText.setText("Score : " + score);
     }
 }
